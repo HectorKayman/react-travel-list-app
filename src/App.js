145dfe4cbb3413ed517b6cb31f21aa533f73fe38
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-23 04:13:54 +0530

    Create basic component skeleton structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+export default function App() {
+  return null;
+}
+
+function Logo() {
+  return null;
+}
+
+function Form() {
+  return null;
+}
+
+function PackingList() {
+  return null;
+}
+
+function Stats() {
+  return null;
+}
